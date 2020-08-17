6db40e4f453c60effa55f14044ee866ba0ef7a70
Nikhil <37348724+Nikhil-K-Singh@users.noreply.github.com>
2020-08-17 20:38:06 +0530

    completed  #Closed :-)

--- main.py
+++ main.py
@@ -20,22 +20,37 @@ cap.set(10, 150)
 # here the color are sored as HSV values
 # with lower and upper limits
 
-#  remember it is in BGR
+#  remember that myColors is in Hmin,Smin,Vmin,Hmax,Smax,Vmax
+
+
 myColors = [
     [5,107,0,19,255,255],
     [133,56,0,159,156,255],
     [57,76,0,100,255,255]
 ]
+
 # Can add / remove as many colors to track 
 
 # below list is to mark the drawing pointer
 # on the screen with the same color
+# below myColorValues is in BGR
 
 myColorValues=[
     [51,153,255],
     [255,0,255],
-    [0,255,0]
+    [0,255,0],
 ]
+
+## make sure that myColors and myColorValues should have the same color reference at teh same index
+# ex index 0 for both is for orange 
+
+
+
+myPoints = []       ## [x,y,colorId]
+
+def drawOnCanvas(myPoints,myColorValues):
+    for point in myPoints:
+        cv2.circle(img,point[0],point[1],10, myColorValues[point[2]],cv2.FILLED)
 
 
 def find_color(img,myColors,myColorValues):
@@ -48,8 +63,11 @@ def find_color(img,myColors,myColorValues):
 
         x,y = getContours(mask)
         cv2.circle(imgResult,(x,y),10,myColorValues[count],cv2.FILLED)
+        if x!=0 and y!=0:
+            newPoints.append([x,y,count])
         count+=1
-
+    return newPoints
+    
 def getContours(img):
     contours, hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     x,y,w,h = 0,0,0,0
@@ -62,10 +80,19 @@ def getContours(img):
             x,y,w,h = cv2.boundingRect(approx)
     return x+w//2,y
 
-    
+
 while True:
     success,img = cap.read()
-    find_color(img,myColors)
-    cv2.imshow("Result",img)
+    imgResult = img.copy()
+    newPoints = find_color(img,myColors,myColorValues)
+
+    if len(newPoints)!=0:
+        for newP in newPoints:
+            myPoints.append(newP)
+
+    if len(myPoints)!=0:
+        drawOnCanvas(myPoints,myColorValues)
+
+    cv2.imshow("Result",imgResult)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
